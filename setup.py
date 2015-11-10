1c837c2199da5d306ee4eef32630fe8c8343204d
Eduardo Santarelli <eduardo.santarell@studio.unibo.it>
2015-11-10 20:16:09 +0100

    File setup completo! Funzionerà?

--- setup.py
+++ setup.py
@@ -1,47 +1,114 @@
 #!/usr/bin/env python2
 # coding: utf-8
-from os import path, getcwd, chdir, fork, execv, waitpid
 
-REQ_FILE = path.join(path.dirname(path.realpath(__file__)), 'requirements.txt')
-PYTHON_LIBS = ['flask', 'flask-script', 'rdflib', 'SPARQLWrapper', 'scrapy']
+import os, sys
+from os import path, fork, execv, waitpid
+
 REPO_DIR = path.dirname(path.realpath(__file__))
+CURRENT_DIR= os.getcwd()
+VIRTUALENV_NAME = 'env'
+REQ_FILE = path.join(path.dirname(path.realpath(__file__)), 'requirements.txt')
+
+def which(program):
+    """ Finds the path of the executable for a given command """
+    def is_exe(fpath):
+        return os.path.isfile(fpath) and os.access(fpath, os.X_OK)
+
+    fpath, fname = os.path.split(program)
+    if fpath:
+        if is_exe(program):
+            return program
+    else:
+        for path in os.environ["PATH"].split(os.pathsep):
+            path = path.strip('"')
+            exe_file = os.path.join(path, program)
+            if is_exe(exe_file):
+                return exe_file
+    return None
+
 
 def checkDir():
     """ Checks that we are in the correct directory """
-    repo_dir = REPO_DIR
-    curr_dir = getcwd()
-    if curr_dir != repo_dir:
-        chdir(repo_dir)
-        print "cd into {}".format(repo_dir)
+    if CURRENT_DIR != REPO_DIR:
+        os.chdir(REPO_DIR)
+        print "cd into {}".format(REPO_DIR)
 
-def makeVirtualenv(name='env'):
+
+def makeVirtualenv(name=VIRTUALENV_NAME):
     """ Creates a default virtualenv """
-    print "Setting up Virtual env"
+    print "=================================="
+    print "Setting up the virtual environment"
+    print "=================================="
+    VIRTUALENV_NAME = name
     pid = fork()
     if pid == 0:
-        execv('/usr/local/bin/virtualenv', ('virtualenv', name)) 
+        virtualenv_executable = which('virtualenv')
+        execv(virtualenv_executable, ('virtualenv', name)) 
     else:
-        return pid
+        status = waitpid(pid, 0)
+        if status[1] != 0:
+            print "ERROR! pip exited with status {}".format(status)
+            sys.exit(1)
+
 
 def installPyLibs(requirements_file=REQ_FILE):
     """ Installs all required libraries in the virtualenv """
-    print "======================================"
+    print "===================================="
     print "Installing required python libraries"
-    print "======================================"
+    print "===================================="
     pid = fork()
     if pid == 0:
-        pip_executable = path.join(REPO_DIR, 'env/bin/pip')
+        pip_executable = path.join(REPO_DIR, VIRTUALENV_NAME,'bin/pip')
         execv(pip_executable,
-                ('pip', 'install', '-r',  requirements_file))
+              ('pip', 'install', '-r',  requirements_file))
     else:
-        waitpid(pid, 0)
+        status = waitpid(pid, 0)
+        if status[1] != 0:
+            print "ERROR! pip exited with status {}".format(status)
+            sys.exit(1)
 
-#======================================
+
+def setupNodeDeps():
+    """ Runs npm install in the static directory """
+    pid = fork()
+    if pid == 0:
+        print "==========================="
+        print "Installing npm dependencies"
+        print "==========================="
+        os.chdir('static')
+        npm_path = which('npm')
+        execv(npm_path, ('npm', 'i'))
+    else:
+        status = waitpid(pid, 0)
+        if status[1] != 0:
+            print "ERROR! npm exited with status {}".format(status)
+            sys.exit(1)
+
+
+def buildStaticFiles():
+    """ Runs gulp to build the static files """
+    pid = fork()
+    if pid == 0:
+        print "==================="
+        print "Starting gulp build"
+        print "==================="
+        os.chdir('static')
+        gulp_path = which('gulp')
+        execv(gulp_path, ('gulp'))
+    else:
+        status = waitpid(pid, 0)
+        if status[1] != 0:
+            print "ERROR! gulp exited with status {}".format(status)
+            sys.exit(1)
+
+
+##################################################
 def main():
     checkDir()
-    venv_pid = makeVirtualenv('env')
-    waitpid(venv_pid ,0)
+    makeVirtualenv('env')
     installPyLibs(REQ_FILE)
+    setupNodeDeps()
+    buildStaticFiles()
 
 
 if __name__ == "__main__":
--- static/client/js/controllers/bottoniCtrl.jsx
+++ static/client/js/controllers/bottoniCtrl.jsx
@@ -2,6 +2,13 @@
 export
 var bottoniCtrl = ['$scope', '$http',
     function($scope, $http) {
-		
-	}
+        $scope.searchAuthor = "";
+        $scope.result = "";
+        $scope.gogo = function() {
+            $http.get('/api/bottoni')
+                .then(function(res) {
+					$scope.result = res.data;
+                });
+        };
+    }
 ];
