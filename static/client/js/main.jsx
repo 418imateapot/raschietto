19250bf25d551d5eb4ef396f85151d80d6a4603a
Eduardo Santarelli <eduardo.santarell@studio.unibo.it>
2015-11-20 20:34:19 +0100

    aggiunto supporto minimo per le animazioni

--- static/client/js/main.jsx
+++ static/client/js/main.jsx
@@ -1,16 +1,19 @@
 /* jshint esnext:true */
 import 'jquery';
 import 'angular';
+import 'angular-animate';
 import {foundation} from 'angular-foundation';
 import {router} from 'angular-ui-router';
 
 import {routes} from './routes.jsx';
 import {mainCtrl, docCtrl, metaCtrl} from './controllers/areaControllers.jsx';
 import {bottoniCtrl} from './controllers/bottoniCtrl.jsx';
+import {loadDocumentService} from './services/loadDocumentService.jsx';
 
-angular.module('teapot', ['mm.foundation', 'ui.router'])
+angular.module('teapot', ['mm.foundation', 'ui.router', 'ngAnimate'])
     .config(routes)
     .controller('docCtrl', docCtrl)
     .controller('metaCtrl', metaCtrl)
     .controller('mainCtrl', mainCtrl)
-    .controller('bottoniCtrl', bottoniCtrl);
+    .controller('bottoniCtrl', bottoniCtrl)
+	.factory('loadDocument', loadDocumentService);
