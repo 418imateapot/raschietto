8571da108ec87fa793789a2f37445797378eb2ca
Eduardo Santarelli <eduardo.santarell@studio.unibo.it>
2015-12-19 23:12:52 +0100

    Risolto un bug in apache_files/install.sh
    Non dovrebbe più esserci il rischio di aggiungere più di una riga "Listen x" in /etc/apache2/ports.conf

--- apache_files/install.sh
+++ apache_files/install.sh
@@ -7,8 +7,16 @@ cd $conf_dir
 # Listen on port 8080
 # enable site
 # restart apache
-cp "010-raschietto.conf" "/etc/apache2/sites-available" \
-&& sed -i '$ i\Listen 8080\n' /etc/apache2/ports.conf \
-&& a2ensite "010-raschietto" \
-&& apache2ctl restart
+cp "010-raschietto.conf" "/etc/apache2/sites-available"
+if [ $? -eq 0 ];
+then
+    # If ports.conf non contiene "Listen 8080"
+    # then aggiungilo sulla riga dopo "Listen 80"
+    # (che in genere c'è)
+    grep -q -F  "Listen 8080" /etc/apache2/ports.conf\
+    || sed -r 's/^(Listen 80)$/\1\nListen 8080/' /etc/apache2/ports.conf \
+    && a2ensite "010-raschietto" \
+    && apache2ctl restart
+fi
+
 
