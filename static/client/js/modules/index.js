1b885150e00b45ac84d86ccf189864dbbac9785e
edusan <eduardo.santarelli@studio.unibo.it>
2015-12-22 03:20:04 +0100

    I don't know what I'm doing [1]

--- static/client/js/modules/index.js
+++ static/client/js/modules/index.js
@@ -1,7 +1,8 @@
 /* jshint esnext: true */
-import './metaArea/metaArea.jsx';
-import './mainArea/mainArea.jsx';
-import './docArea/docArea.jsx';
+import {metaArea} from './metaArea/metaArea.jsx';
+import {mainArea} from './mainArea/mainArea.jsx';
+import {docArea} from './docArea/docArea.jsx';
 
 export
 var teapotAreas = angular.module('teapot.areas', ['metaArea', 'docArea', 'mainArea']);
+console.log(mainArea);
