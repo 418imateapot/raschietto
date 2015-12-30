87dd9a73b2732940a64c4e5c4326803f2fbc3d5b
Eduardo Santarelli <eduardo.santarell@studio.unibo.it>
2015-12-30 21:53:42 +0100

    Cool navbar!

--- static/client/js/modules/raschiettoNavbar/raschiettoNavController.jsx
+++ static/client/js/modules/raschiettoNavbar/raschiettoNavController.jsx
@@ -3,6 +3,38 @@
  * Controller per la metaArea
  */
 export
-var raschiettoNavCtrl = ['$scope', 'annotationService',
-    function($scope, annotationService) {}
+var raschiettoNavCtrl = ['$scope', '$state', '$sce',
+    function($scope, $state, $sce) {
+
+
+        var readerModeString = $sce.trustAsHtml('<i class="fa fa-book"></i> Modalità Reader');
+        var annotatorModeString = $sce.trustAsHtml('<i class="fa fa-pencil"></i> Modalità Annotator');
+        var tutorialModeString = $sce.trustAsHtml('<i class="fa fa-graduation-cap"></i> Tutorial</a>');
+
+        $scope.currentState = $state;
+        $scope.$watch('currentState.current.name', function(newState, oldState) {
+            switch (newState) {
+                case 'annotator':
+                    $scope.activeMode = annotatorModeString;
+                    $scope.mode2 = readerModeString;
+                    $scope.mode3 = tutorialModeString;
+                    $scope.color = 'annotator-red';
+                    break;
+                case 'home':
+                    $scope.activeMode = tutorialModeString;
+                    $scope.mode2 = readerModeString;
+                    $scope.mode3 = annotatorModeString;
+                    $scope.color = 'tutorial-green';
+                    break;
+                case 'reader':
+                default:
+                    $scope.activeMode = readerModeString;
+                    $scope.mode2 = annotatorModeString;
+                    $scope.mode3 = tutorialModeString;
+                    $scope.color = 'reader-blue';
+                   break;
+            }
+        });
+
+    }
 ];
