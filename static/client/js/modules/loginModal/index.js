6fc9abfbffca55317a1c177e4f400bfb3957b441
Burlollone <twuser@raschibox>
2016-01-03 14:23:49 +0100

    Implementazione userservice per il login

--- static/client/js/modules/loginModal/index.js
+++ static/client/js/modules/loginModal/index.js
@@ -2,9 +2,10 @@
 
 import {loginModal} from './loginModal.js';
 import {loginModalCtrl} from './loginModalCtrl.js';
+import {UserService} from './UserService.js';
 
 var login = angular.module('teapot.ui.login', []);
 login.service('loginModal', loginModal);
 login.controller('loginModalCtrl', loginModalCtrl);
-
+login.service('UserService', UserService);
 export {login};
